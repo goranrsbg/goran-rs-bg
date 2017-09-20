c09b7e8b3df991b71fa4a75778322a789df5146b
Goran Cvijanovic <goran.rs.bg@gmail.com>
2017-09-20 22:13:32 +0200

    ...

--- src/app/gate-form/gate-form.component.ts
+++ src/app/gate-form/gate-form.component.ts
@@ -6,55 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gate-form.component.css']
 })
 export class GateFormComponent implements OnInit {
-  
-  private _text: string;
+
   private _PASSWORD: string = "GaMeTiMe";
+  private lastCode: string;
   private _code: string;
   private _result: string;
-  
+  private mBox: HTMLElement;
+
   constructor() {
-    this._text = "This is string from GateFormComponent class.";
+    this.lastCode = "";
     this._code = "";
-    this._result = "";
+    this._result = "message";
   }
 
   ngOnInit() {
+    this.mBox = document.getElementById("message");
+    this.mBox.style.visibility = "hidden";
   }
 
   onEnter() {
       if(this._code == this._PASSWORD) {
         this._result = this._code;
       } else {
-        this._result = "F A L S E";
-        this._code = "";
+        if(!running) {
+          this.mBox.style.visibility = "visible";
+          console.log("HERE");
+          this._result = "F A L S E";
+          animateOpacity(this.mBox);
+          this._code = "";
+          this.lastCode = "";
+        }
       }
   }
 
-  get code(): string {
-    return this._code;
+  onKey() {
+    if(this._code.length > this.lastCode.length) {
+      
+      this.lastCode = this._code;
+    } else if(this._code.length < this.lastCode.length) {
+      this.lastCode = this._code;
+    }
   }
 
-  set code(text: string) {
-    this._code = text;
-  }
+}
 
-  get text(): string {
-    return this._text;
-  }
+let running: boolean = false;
 
-  set text(text: string) {
-    this._text = text;
+function animateOpacity(e: HTMLElement) {
+  let opc: number = 2;
+  running = true;
+  anim();
+  function anim() {
+    if(opc < 0) {
+      e.style.visibility = "hidden";
+      e.style.opacity = "1.0";
+      running = false;
+    } else {
+      opc = opc - 0.1;
+      if(opc < 1)
+        e.style.opacity = opc + "";
+      setTimeout(anim, 100);
+    }
   }
-
-  get result(): string {
-    return this._result;
-  }
-
-  set result(text: string) {
-    this._result = text;
-  }
-  get PASSWORD(): string {
-    return this._PASSWORD;
-  }
-
 }
